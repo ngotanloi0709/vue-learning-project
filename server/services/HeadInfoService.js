585cce2b17abfbdb9b51532a34f37fd35b507c66
Loi Ngo <ngotanloi0709@gmail.com>
2025-06-30 05:03:52 +0700

    feature head info forward log

--- server/services/HeadInfoService.js
+++ server/services/HeadInfoService.js
@@ -5,8 +5,23 @@ export class HeadInfoService {
 
     async getPageTitle(url) {
         const page = await this.puppeteerService.createOptimizedPage()
+        const redirectHistory = [url]
 
         try {
+            page.on('response', (response) => {
+                const request = response.request()
+                if (request.redirectChain().length > 0) {
+                    const lastRedirect = request.redirectChain().at(-1)
+
+                    if (lastRedirect) {
+                        const redirectedUrl = lastRedirect.url()
+                        if (!redirectHistory.includes(redirectedUrl)) {
+                            redirectHistory.push(redirectedUrl)
+                        }
+                    }
+                }
+            })
+
             await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 20000 })
 
             const headInfo = await page.evaluate(() => {
@@ -14,7 +29,7 @@ export class HeadInfoService {
                 return { title }
             })
 
-            return headInfo
+            return { ...headInfo, redirectHistory }
         } catch (error) {
             console.error(`Lỗi khi lấy thông tin đầu trang từ ${url}:`, error)
             throw new Error(`Không thể lấy thông tin đầu trang từ ${url}: ${error.message}`)
